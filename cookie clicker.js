3a31b51cc267bbf56c4eeb931babba89da531e5c
Vulcran22 <liam.hubrich@gmail.com>
2024-01-16 12:31:30 +0100

    cookieclicker

--- cookie clicker.js
+++ cookie clicker.js
@@ -1,6 +1,16 @@
 var clicks = 0;
+var multiplikator = 0;
 
 function onClick() {
   clicks += 1;
+  clicks += 1*multiplikator;
   document.getElementById("clicks").innerHTML = clicks;
 }
+
+function clickme() {
+    multiplikator +=2;
+    document.getElementById("multiplikator").innerHTML = multiplikator;
+}
+
+document.getElementById("Upgrade1").addEventListener("click",clickme);
+

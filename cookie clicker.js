21944996e09ed4a610d519e25e4235948a2050f5
Vulcran22 <liam.hubrich@gmail.com>
2024-01-19 12:00:57 +0100

    cookie

--- cookie clicker.js
+++ cookie clicker.js
@@ -1,16 +1,27 @@
 var clicks = 0;
-var multiplikator = 0;
+var multiplikator = 0;s
 
 function onClick() {
   clicks += 1;
-  clicks += 1*multiplikator;
+  clicks += 1 * multiplikator;
   document.getElementById("clicks").innerHTML = clicks;
 }
 
 function clickme() {
-    multiplikator +=2;
-    document.getElementById("multiplikator").innerHTML = multiplikator;
+  multiplikator += 2;
+  document.getElementById("multiplikator").innerHTML = multiplikator;
 }
+document.getElementById("Upgrade1").addEventListener("click", clickme)
 
-document.getElementById("Upgrade1").addEventListener("click",clickme);
+
+function cookiekosten() {
+  kosten -= 100;
+  document.getElementById("kosten").addEventListener.innerHTML = kosten;
+
+  if (clicks < 100)
+    document.getElementById("Upgrade1").disabled = true;
+  else if (clicks > 100)
+    document.getElementById("Upgrade1").disabled = false;
+}
+document.getAnimations("Upgrade1").addEventListener("click",cookiekosten)
 
